988b13d3aa3cc1ceba04e1489d705038f5917c88
Hasitha Gamage <hasitha@rocketbots.io>
2023-04-27 13:01:55 +0800

    fix: add logs for release and tags

--- index.js
+++ index.js
@@ -96,6 +96,17 @@ const main = async () => {
             }
         }
 
+        const releasesList = await octokit.rest.repos.listReleases({
+            owner,
+            repo
+        });
+
+        const tagsList = await octokit.rest.repos.listTags({
+            owner,
+            repo,
+        });
+
+
         /**
          * Create a comment on the PR with the information we compiled from the
          * list of changed files.
@@ -109,6 +120,8 @@ const main = async () => {
         - ${diffData.changes} changes \n
         - ${diffData.additions} additions \n
         - ${diffData.deletions} deletions \n
+        -- releasesList -- ${JSON.stringify(releasesList.data)} \n
+        -- tagsList -- ${JSON.stringify(tagsList.data)} \n
       `
         });
 
