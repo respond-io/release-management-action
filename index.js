ad8db4ba4bc5b67cf986bd7b85ad445e33518da5
Hasitha Gamage <hasitha@rocketbots.io>
2023-05-03 01:29:29 +0800

    fix: update code

--- index.js
+++ index.js
@@ -125,6 +125,9 @@ const main = async () => {
         console.log('commits...>>', JSON.stringify(filterCommits(compare.data.commits)));
         console.log('files...>>', JSON.stringify(filterFiles(compare.data.files)));
 
+        const commitsDiff = filterCommits(compare.data.commits);
+        const changedFilesList = filterFiles(compare.data.files);
+
 
 
 
@@ -166,52 +169,13 @@ const main = async () => {
         }
 
         const changelogDataSet = {
-            "version": "3.2.0",
-            "previous_version": "1.3.0",
-            "org": "respond-io",
-            "repo": "utils-pkg",
-            "date": "2023-04-18",
-            "body": "This is my first post!",
-            "features": [{
-                "commit_name": "add support for formatting whatsapp cIDs",
-                "commit_hash": "f572ca42583fad58547ea34d93d55f703bcc0be4",
-                "compact_commit_hash": "f572ca4"
-            }, {
-                "commit_name": "added isUpdatabled function for backward compability",
-                "commit_hash": "825eee4b609b5db56b981d16aca27fb70d7211d9",
-                "compact_commit_hash": "825eee4"
-            }],
-            "bug_fixes": [{
-                "commit_name": "add missing space",
-                "commit_hash": "0dc2eaf21c49a684d99b4e3115040e4c5b9f2d7a",
-                "compact_commit_hash": "0dc2eaf"
-            }, {
-                "commit_name": "added more checks on whatsapp cId for various countries",
-                "commit_hash": "04c52d37d8446d07eb37b70a8703b4a66828a3f0",
-                "compact_commit_hash": "04c52d3"
-            }],
-            "other_commits": [{
-                "commit_name": "add missing space",
-                "commit_hash": "0dc2eaf21c49a684d99b4e3115040e4c5b9f2d7a",
-                "compact_commit_hash": "0dc2eaf"
-            }, {
-                "commit_name": "added more checks on whatsapp cId for various countries",
-                "commit_hash": "04c52d37d8446d07eb37b70a8703b4a66828a3f0",
-                "compact_commit_hash": "04c52d3"
-            }],
-            "affected_areas": [{
-                    "entity": "messaging",
-                    "type": "ECS"
-                },
-                {
-                    "entity": "billing",
-                    "type": "Lambda"
-                },
-                {
-                    "entity": "infra/cloudformation/infrastructure/13-ses-email-pipeline.yml",
-                    "type": "Infrastructure"
-                }
-            ]
+            version: "3.2.0",
+            previous_version: "3.1.0",
+            org: owner,
+            repo,
+            date: "2023-04-18",
+            ...commitsDiff,
+            affected_areas: changedFilesList 
         };
 
         const changeLog = await ChangeLog.generateChangeLogContent(octokit, owner, repo, changelogDataSet);
